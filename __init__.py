e6b1003edcd8e2ccd23468e24189495622b721bb
chromoly <chromolytube@gmail.com>
2016-12-29 21:56:23 +0900

    version: (1, 8, 3)

--- __init__.py
+++ __init__.py
@@ -20,7 +20,7 @@
 bl_info = {
     'name': 'CTools',
     'author': 'chromoly',
-    'version': (1, 8, 2),
+    'version': (1, 8, 3),
     'blender': (2, 78, 0),
     'location': '',
     'description': 'Collection of add-ons',
